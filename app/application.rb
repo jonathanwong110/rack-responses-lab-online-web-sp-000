90b4c3be55af5a255c3ac94beee2e8938ddbc44c
Jonathan Wong <jonathan.wong110@gmail.com>
2019-06-21 14:47:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class App
   
   def call(env)
     resp = Rack::Response.new
-    if Time.now < 2019-06-21-12:00:00
+    if Time.now.hour
       resp.write "Morning"
     else Time.now < 2019-06-21-12:00:00
       resp.write "Afternoon"
